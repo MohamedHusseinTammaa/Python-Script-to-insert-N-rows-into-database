bb1606b8fe2274c7f947fc776e6a16d8d4b21484
mohamed hussein <muhamed.tammaa@gmail.com>
2025-04-14 10:48:00 +0200

    edit file path

--- problems.py
+++ problems.py
@@ -1,7 +1,7 @@
 import random
 
 string = "abcdefghijklmnopqrstuvwxyz123456789"
-with open("E:\\programming\\python\\file.txt", "w") as f:
+with open("file.txt", "w") as f:
     f.write("INSERT INTO temp (t, name) VALUES\n")
     for i in range (1,100000):
         name = string[random.randint(0, len(string)-1)] + string[random.randint(0, len(string)-1)] + string[random.randint(0, len(string)-1)]
